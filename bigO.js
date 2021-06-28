a303b4a9d23b168d61677521525ad20959264cf5
auleki <kodagiwa@gmail.com>
2021-06-28 18:07:58 +0100

    removed recursive countdown

--- bigO.js
+++ bigO.js
@@ -34,3 +34,33 @@ const startFast = new Date()
 console.log(uniqueFast(randomArray))
 const endFast = new Date()
 console.log(`fast n finished in ${endFast - startFast}ms.`)
+
+/* 
+
+1. divide n by base 
+2. update value of argument with result
+3. increment counter
+4. if arg value is less than or equal to one, finish
+
+*/
+console.log('-----------------------------')
+const findLog = (base, num) => {
+  let logValue = 0
+  let argValue = num
+  while (argValue >= 1) {
+    argValue /= base
+    logValue++
+    console.log('Log Value now ' + logValue)
+  }
+  return `Log Value is ${logValue}`
+}
+
+const gaga = n => {
+  if (n <= 1) {
+    console.log('hooray')
+    return
+  }
+  console.log(n)
+  gaga(n / 2)
+}
+gaga(30)
